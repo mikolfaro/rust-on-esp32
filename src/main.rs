f1ff8de78c77d64c30e5c49055940fe4b81f675a
Mikol Faro <mikol.faro@gmail.com>
2023-10-11 14:38:19 +0200

    Dummy API call to toggle LED

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 mod wifi;
 
 use embedded_svc::http::Method;
-use esp_idf_hal::prelude::Peripherals;
+use esp_idf_hal::{gpio::PinDriver, prelude::Peripherals};
 use esp_idf_svc::http::server::{Configuration, EspHttpServer};
 use esp_idf_svc::{
     eventloop::EspSystemEventLoop, nvs::EspDefaultNvsPartition, timer::EspTaskTimerService,
 };
 use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
 use log::*;
+use std::sync::{Arc, Mutex};
 use std::thread::sleep;
 use std::time::Duration;
 
@@ -19,6 +20,8 @@ fn main() {
     esp_idf_sys::link_patches();
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
+
+    // 41 40 39
 
     let peripherals = Peripherals::take().unwrap();
     let sysloop = EspSystemEventLoop::take().unwrap();
@@ -33,10 +36,20 @@ fn main() {
 
     let mut server = EspHttpServer::new(&Configuration::default()).unwrap();
 
+    let red_led_pin = Arc::new(Mutex::new(PinDriver::output(peripherals.pins.gpio39).unwrap()));
+    let green_led_pin = Arc::new(Mutex::new(PinDriver::output(peripherals.pins.gpio40).unwrap()));
+    let blue_led_pin = Arc::new(Mutex::new(PinDriver::output(peripherals.pins.gpio41).unwrap()));
+
     server
-        .fn_handler("/", Method::Get, |req| {
+        .fn_handler("/", Method::Get, move |req| {
             let mut resp = req.into_ok_response().unwrap();
             resp.write("Hell, World!".as_bytes()).unwrap();
+
+            red_led_pin.lock().unwrap().toggle().unwrap();
+            green_led_pin.lock().unwrap().toggle().unwrap();
+            blue_led_pin.lock().unwrap().toggle().unwrap();
+
+            info!("Toggle Pin");
 
             Ok(())
         })
