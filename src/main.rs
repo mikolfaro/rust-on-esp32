d8c7a97a15ae2923399ea67379d7d4e90ef81288
Mikol Faro <mikol.faro@gmail.com>
2023-10-11 14:12:44 +0200

    Add dummy pin

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,14 @@ mod wifi;
 
 use embedded_svc::http::Method;
 use esp_idf_hal::prelude::Peripherals;
+use esp_idf_svc::http::server::{Configuration, EspHttpServer};
 use esp_idf_svc::{
     eventloop::EspSystemEventLoop, nvs::EspDefaultNvsPartition, timer::EspTaskTimerService,
 };
-use esp_idf_svc::http::server::{Configuration, EspHttpServer};
 use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
 use log::*;
-use std::time::Duration;
 use std::thread::sleep;
+use std::time::Duration;
 
 use crate::wifi::wifi;
 
@@ -19,8 +19,6 @@ fn main() {
     esp_idf_sys::link_patches();
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
-
-    info!("Hello World!");
 
     let peripherals = Peripherals::take().unwrap();
     let sysloop = EspSystemEventLoop::take().unwrap();
@@ -35,12 +33,14 @@ fn main() {
 
     let mut server = EspHttpServer::new(&Configuration::default()).unwrap();
 
-    server.fn_handler("/", Method::Get, |req| {
-        let mut resp = req.into_ok_response().unwrap();
-        resp.write("Hell, World!".as_bytes()).unwrap();
+    server
+        .fn_handler("/", Method::Get, |req| {
+            let mut resp = req.into_ok_response().unwrap();
+            resp.write("Hell, World!".as_bytes()).unwrap();
 
-        Ok(())
-    }).unwrap();
+            Ok(())
+        })
+        .unwrap();
 
     loop {
         sleep(Duration::from_secs(1));
